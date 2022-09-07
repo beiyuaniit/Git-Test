a7fe1b9f9ba583c26be3abb142ffd609fecb4684
Local <beiyuan@me.com>
2022-09-07 16:41:14 +0800

    first commit

---
+++ src/main/java/Test/HelloWorld.java
@@ -0,0 +1,14 @@
+package Test;
+
+/**
+ * @author: beiyuan
+ * @className: HelloWorld
+ * @date: 2022/9/7  16:38
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("rain");
+        System.out.println("bow");
+    }
+}
