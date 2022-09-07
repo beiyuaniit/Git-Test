ea286cf363f791408e1a79cac258d704f3eb84fa
Local <beiyuan@me.com>
2022-09-07 16:56:40 +0800

    second commit

--- src/main/java/Test/HelloWorld.java
+++ src/main/java/Test/HelloWorld.java
@@ -10,5 +10,6 @@ public class HelloWorld {
         System.out.println("hello world");
         System.out.println("rain");
         System.out.println("bow");
+        System.out.println("shine");
     }
 }
